6bfdac9850e65bf8fab2767b5f9ea544e7bc7a7c
nidhi <nidhi7485@gmail.com>
2023-03-22 14:44:19 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import data from './data.js'
+import path from 'path'
+
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 const app = express()
@@ -38,6 +39,12 @@ app.get('/api/products/slug/:slug', (req, res) => {
     res.status(400).send({ message: 'product not found!' })
   }
 })
+
+const __dirname = path.resolve()
+app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+)
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message })
 })
